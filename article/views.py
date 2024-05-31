d55abfeef08046f549176f10caddae7312cc2a69
moyudaduizhang <2790762491@qq.com>
2024-05-31 22:41:48 +0800

    create a new reposity

--- article/views.py
+++ article/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import ArticlePost
+# 引入刚才定义的ArticlePostForm表单类
+from .forms import ArticlePostForm
+# 引入User模型
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -20,3 +24,34 @@ def article_detail(request, id):
         'article': article
     }
     return render(request, 'article/detail.html', context)
+
+
+# 写文章的视图
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            # 如果你进行过删除数据表的操作，可能会找不到id=1的用户
+            # 此时请重新创建用户，并传入此用户的id
+            new_article.author = User.objects.get(id=1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect("article:article_list")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = {'article_post_form': article_post_form}
+        # 返回模板
+        return render(request, 'article/create.html', context)
